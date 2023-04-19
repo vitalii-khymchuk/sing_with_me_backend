f8b4cc75b79a5648f1eaa74c3012a0002c5945ef
Vitalii Khymchuk <vitalii.workbox001@gmail.com>
2023-04-19 14:09:36 +0300

    add urlencoded to app

--- app.js
+++ app.js
@@ -4,20 +4,21 @@ const cors = require("cors");
 
 const {
   recognitionRouter,
-  userLibrariesRouter,
+  userLibraryRouter,
   googleAuthRouter,
 } = require("@routes/api");
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
+app.use(express.urlencoded({ extended: true }));
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use("/", express.static("./public"));
 app.use("/api/v1", recognitionRouter);
 app.use("/api/v1", googleAuthRouter);
-app.use("/api/v1", userLibrariesRouter);
+app.use("/api/v1", userLibraryRouter);
 
 app.use((req, res) => {
   res.status(404).json({ code: 404, message: "Not found" });
